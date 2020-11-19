ececfd713c1d0364077d741cd49b481e199c1b5c
ghgfhfg2 <ghgfhfg2@naver.com>
2020-11-19 23:08:21 +0900

    create context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { createContext } from "react";
+
+const UserContext = createContext("untitle");
 
 function App() {
   return (
     <div className="App">
-      hello
+      <UserContext.Provider value="mike">
+        <Sub />
+      </UserContext.Provider>
     </div>
+  );
+}
+
+function Sub() {
+  return (
+    <>
+      <Sub2 />
+    </>
+  );
+}
+
+function Sub2() {
+  return (
+    <>
+      <UserContext.Consumer>{(name) => <p>{name}</p>}</UserContext.Consumer>
+    </>
   );
 }
 
